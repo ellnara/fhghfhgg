51d749f4dbb77aa075a4cdbd63da078f5991372d
ellnara <100730699+ellnara@users.noreply.github.com>
2022-03-31 19:36:05 +0400

    added book task

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group = new Group();
+            group.AddStudent(new Student("elnare", "semedova", 19, 1000));
+            group.AddStudent(new Student("elnare", "semedova", 19, 1));
+            group.AddStudent(new Student ("elnare", "semedova", 19, 2));
+
+            group.GetAllStudents();
+        }
+    }
+}
